1010dc49df2bddb82e8b09e64effd7cecd4e05ae
Erik Slovak <erik.slovak@technologystudio.sk>
2023-07-24 16:18:46 +0200

    Revert "chore: fix eslint issues"
    This reverts commit 81ed6b86fbb623265df35b2bae018ef8f5e15fe0.

--- src/Redux/Model/ResettableRedux.ts
+++ src/Redux/Model/ResettableRedux.ts
@@ -31,7 +31,8 @@ export const resettableReducer = <STATE, ACTION extends Action> (originalReducer
   if (typeof originalReducer !== 'function') {
     throw new Error('A reducer is required.')
   }
-  const resetState = originalReducer(undefined, {} as unknown as ACTION)
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const resetState = originalReducer((undefined as any), ({} as any))
 
   const reducer = (state = resetState, action: ACTION): STATE => action.type === types.RESET
     ? resetState
